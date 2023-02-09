5b6e96ccf4b86cd76611003d9cb5a3d8aa8af29a
CGoddard1 <goddard.cole@gmail.com>
2023-02-09 18:29:35 -0500

    added first funtion + password length

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  var passLength = window.prompt("Enter the length of password you would like");
+
+if (passLength < 8 || passLength > 128) {
+  window.alert("Your password MUST be between 8 and 128 characters.")
+  return;
+}
+
 var length = window.prompt("choose password length between 8 and 128 characters")
 
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -7,12 +16,18 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
-var confirmLength ="";
-var confirmUpperCase="";
-var confirmLowercase="";
-var confirmSpecialCharacter="";
-var confirmNumbers="";
-var passwordText="";
+// function generatePassword(){
+//   var length = 8;
+//   char = "abcdefghijklmnopqrstuvwkyz";
+//   num = "1234567890";
+//   passwordText = [""];
+//    for(i=0;i < length; i++){
+//      passwordText += char.charAt(Math.floor(Math.random() * char.length));
+//    }
+//    return passwordText;
+//   };
+  
+//   console.log(generatePassword());
   
 // Write password to the #password input
 function writePassword() {
