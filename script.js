ccfdad02829b4ed495aaf20d0cfa11bfbc44512b
CGoddard1 <goddard.cole@gmail.com>
2023-02-09 17:45:09 -0500

    added letters, numbers and special characters

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var length = window.prompt("choose password length between 8 and 128 characters")
 
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+var confirmLength ="";
+var confirmUpperCase="";
+var confirmLowercase="";
+var confirmSpecialCharacter="";
+var confirmNumbers="";
+var passwordText="";
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,9 +28,9 @@ generateBtn.addEventListener("click", writePassword);
 
 // write a function that takes in use input (based on our acceptance criteria) and returns a password string
 function generatePassword() {
-  var passwordString = "";
+  var passwordText = "";
 
   // do stuff
 
-  return passwordString;
+  return passwordText;
 }
