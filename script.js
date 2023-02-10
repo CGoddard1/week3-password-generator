dc275bc6fef9ad7e21af05f811fb036801f1e93d
CGoddard1 <goddard.cole@gmail.com>
2023-02-10 15:30:52 -0500

    added if statements for empty array

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//prompt to ask for password length
 function generatePassword() {
   var passLength = window.prompt("Enter the length of password you would like");
 
+//return prompt if length is too short or too long
 if (passLength < 8 || passLength > 128) {
   window.alert("Your password MUST be between 8 and 128 characters.")
   return;
@@ -16,14 +18,30 @@ var numbers = window.confirm("Would you like your password to include numbers?")
 var specialchar = window.confirm("Would you like your password to include special characters?")
 
 //list of characters for the password
-
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
+//empty array for user's choices
+var passwordContainer = [];
 
+//If a user wants these characters, these IF statements will be pushed into the empty Array
+if (lowercase === true) {
+  passwordContainer.push(lowercaseList);
+}
 
+if (uppercase === true) {
+  passwordContainer.push(uppercaseList);
+}
+
+if (numbers === true) {
+  passwordContainer.push(numbersList);
+}
+
+if (specialChar === true) {
+  passwordContainer.push(specialCharList);
+}
 
 // Write password to the #password input
 function writePassword() {
